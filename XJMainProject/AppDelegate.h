51dc622eb05c7009f63787fac0cf60cd458eb2a8
江鑫 <jiangxin@jiangxindeMacBook-Air.local>
2018-04-12 15:12:45 +0800

    tijiao

---
+++ XJMainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJMainProject
+//
+//  Created by 江鑫 on 2018/4/12.
+//  Copyright © 2018年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJMainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJMainProject
+//
+//  Created by 江鑫 on 2018/4/12.
+//  Copyright © 2018年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
